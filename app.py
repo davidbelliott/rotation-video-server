3ec82ddacbc1d56f5acc68fdb6fcfa90946f4f7d
David Elliott <delliott@caltech.edu>
2018-10-03 10:07:41 -0700

    Add cookie to prevent reloading and voting multiple times

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, join_room, leave_room, send, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 current_room_index = 0
 NUM_PLAYER_ROOMS = 6
+current_users = []
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @socketio.on('connect')
-def test_connect():
+def connect():
     print('Client connected')
 
 @socketio.on_error()
@@ -19,22 +20,30 @@ def error_handler(e):
     print('Error: {}'.format(e))
 
 @socketio.on('disconnect')
-def test_connect():
+def disconnect():
     print('Client disconnected')
+    print('Removing user: {}'.format(request.sid))
+    current_users.remove(request.sid)
 
 @socketio.on('join')
 def on_join(data):
     global current_room_index
+    global current_users
     print('Joining')
     is_player = data['is_movie_player']
     if is_player:
         print('Movie player')
         join_room('movie_players')
+        emit('users', current_users, room=request.sid)
     else:
+        voted = data['voted']
         join_room('users')
         join_room(str(current_room_index))
         print('Joined room index: {}'.format(current_room_index))
         current_room_index = (current_room_index + 1) % NUM_PLAYER_ROOMS
+        emit('user_joined', {'id': request.sid, 'voted': voted}, room='movie_players')
+        current_users.append(request.sid)
+        print('User joined: {}, {}'.format(request.sid, voted))
 
 @socketio.on('leave')
 def on_leave(data):
@@ -45,6 +54,9 @@ def on_leave(data):
         leave_room('movie_players')
     else:
         leave_room('users')
+        emit('user_left', request.sid, room='movie_players')
+        current_users.remove(request.sid)
+        print('User left: {}'.format(request.sid))
 
 @socketio.on('cast_vote')
 def handle_cast_vote(vote):
